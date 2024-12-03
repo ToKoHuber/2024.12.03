82ec1fc716db5eca79f556902aed90e6afb865cd
altantogoo <82674692+ToKoHuber@users.noreply.github.com>
2024-12-04 01:34:29 +0800

    second update

---
+++ Homework2.js
@@ -0,0 +1,16 @@
+Input = "Hello world! I am Fullstack Developer";
+InputArray = Input.split(" ");
+
+// InputArray.pop();
+// InputArray.push(InputArray[0]);
+// InputArray.shift();
+// InputArray.unshift("InputArray.length");
+
+// // console.log({ InputArray });
+// console.log({ InputArray });
+
+//use replace command
+
+InputArray.replace(InputArray[0], InputArray.length);
+
+console.log(InputArray);
